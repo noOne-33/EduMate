1aa72f4732412e8c203211e0f19b108483bbf7bd
noOne-33 <nazirsahin4545@gmail.com>
2025-10-26 19:09:07 +0600

    Initial commit without node_modules

--- .next/static/chunks/src_app_login_page_tsx_5ce2d41e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_70823114._.js",
-  "static/chunks/src_components_3dc93880._.js"
-],
-    source: "dynamic"
-});
